b13b2998cbbee6da95cb240644b53a43f61e2818
Zen <clonebmn2itt@gmail.com>
2019-08-26 15:41:25 +0000

    refactor: set token in environment variable

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TOKEN = "NjA0NzIzODkxNTAzNjI4MzI4.XWO4uQ.ImwnBryfwYqjxIAUNSRnwGVq8hM";
+const TOKEN = process.env.TOKEN || "";
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, channel } = require('./config/bot');
@@ -6,10 +6,10 @@ const processCommand = require("./command");
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity("Search", { type: "LISTENING" });
-    //   const generalChannel = client.channels.get(channel.general);
-    //   generalChannel.send("Schedule bot ready!");
-    //   const botImage = new Discord.Attachment("https://ih1.redbubble.net/image.582237006.5163/flat,1000x1000,075,f.u1.jpg");
-    //   generalChannel.send(botImage)
+    const generalChannel = client.channels.get(channel.general);
+    generalChannel.send("Schedule bot ready!");
+    const botImage = new Discord.Attachment("https://ih1.redbubble.net/image.582237006.5163/flat,1000x1000,075,f.u1.jpg");
+    generalChannel.send(botImage)
 });
 
 client.on('message', (receivedMessage) => {
