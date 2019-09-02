c189dab24d37d1d1d09b4d08507796bb8c51e8ee
Zen <clonebmn2itt@gmail.com>
2019-09-02 11:58:21 +0000

    fix: Catch error when exec commands, events.

--- commands/index.js
+++ commands/index.js
@@ -5,10 +5,15 @@ module.exports = function(client) {
         const cmd = splitCommand[0].toLowerCase(); // The first word directly after the exclamation is the command
         const args = splitCommand.slice(1); // All other words are arguments/parameters/options for the command
 
-        console.log("Command: " + cmd);
-        console.log("Arguments: " + args);
+        // console.log("Command: " + cmd);
+        // console.log("Arguments: " + args);
         if (!client.commands.has(cmd)) return message.channel.send("Lệnh không hỗ trợ");
         const command = client.commands.get(cmd);
-        return command.run(client, message, args);
+        try {
+            return command.run(client, message, args);
+        }
+        catch (e) {
+            client.log(e.stack);
+        }
     }
 };
--- events/index.js
+++ events/index.js
@@ -1,6 +1,13 @@
 module.exports = function(client) {
     return function(message) {
         const _event = client.events.get(message.author.id);
-        if(client.eventers.has(_event.type)) return client.eventers.get(_event.type).run(client, message, _event)
+        if (client.eventers.has(_event.type)) {
+            try {
+                return client.eventers.get(_event.type).run(client, message, _event)
+            }
+            catch (e) {
+                client.log(e.stack);
+            }
+        }
     }
 };
