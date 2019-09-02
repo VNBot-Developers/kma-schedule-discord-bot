63a97fc737fb0ed57367c5afe5828123474435e7
Zen <clonebmn2itt@gmail.com>
2019-09-02 14:48:44 +0000

    refactor(login): Use AwaitMessage

--- events/login.js
+++ events/login.js
@@ -1,15 +1,33 @@
+function handler(client, message, email, password) {
+    message.channel.send(`Bạn đăng nhập với tài khoản ${email} mật khẩu ${password}`);
+}
 exports.run = function(client, message, _event) {
     if (!_event.data.email) {
-        _event.data.email = message.content;
-        message.channel.send("Nhập password!");
-        client.events.set(message.author.id, _event);
-        return;
-    }
-    if (!_event.data.password) {
-        _event.data.password = message.content;
-        message.channel.send(`${_event.data.email} - ${_event.data.password}`);
-        message.delete();
+        const email = message.content;
+        message.channel.send("Nhập password!")
+            .then(function() {
+                const filter = m => message.author.id === m.author.id;
+                message.channel.awaitMessages(filter, { time: 10 * 1000, maxMatches: 1, errors: ['time'] })
+                    .then(function(messages) {
+                        const password = messages.first().content;
+                        handler(client, message, email, password)
+                    })
+                    .catch(() => {
+                        message.channel.send('Hết hạn nhập vui lòng đăng nhập lại!');
+                    });
+            })
+            .catch((e) => {
+                message.channel.send(e.stack);
+            });
         client.events.delete(message.author.id);
         return;
     }
+    if (!_event.data.password) {
+        const email = _event.data.email;
+        const password = message.content;
+        handler(client, message, email, password)
+        client.events.delete(message.author.id);
+        return;
+    }
+
 }
