5341fbfe2d17934fb0fe438d8c4b4e1b8c60d25f
Linh <clonebmn2itt@gmail.com>
2019-10-27 17:03:51 +0700

    refactor: Delete unused test file

--- test/login.js
+++
@@ -1,9 +0,0 @@
-const User = require("../models/user");
-const Account = require("../models/account");
-
-const user = new User({displayName: "Trần Đức Cường", discordId: "123"});
-
-(async function(){
-    const a = await user.save();
-    console.log(a)
-})()
--- test/mocha.js
+++
@@ -1,7 +0,0 @@
-const mocha = require("mocha");
-const assert = require('assert');
-describe("Mo ta tester", function(){
-    it("1 + 1 = 3", function(){
-          assert.equal(1 + 1 , 2)
-    })
-})
