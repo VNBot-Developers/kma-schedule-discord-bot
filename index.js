1b93082e8fcac8e4845a473d4525fdd66a94cda7
Linh <clonebmn2itt@gmail.com>
2019-10-27 18:30:45 +0700

    fix: audit fix

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const { DATABASE_STRING } = process.env;
 mongoose.connect(DATABASE_STRING, {
     useNewUrlParser: true,
     useCreateIndex: true,
+    useUnifiedTopology: true,
 });
 mongoose.connection
     .once('open', function() {
